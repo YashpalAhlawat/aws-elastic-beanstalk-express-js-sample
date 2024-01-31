8983fc410395d99b390df559cb504b406da20880
Yashpal Ahlawat <yashpalahlawat8@gmail.com>
2024-01-31 23:00:52 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Here is the first AWS Hands on training started. Let\'s see how far we go'));
+app.get('/', (req, res) => res.send('Here is the first AWS Hands on training started. Make some change'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
